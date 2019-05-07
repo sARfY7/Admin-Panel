3c2b5ea5faf2811f570d0932b5839a16c7ddb30d
Sarfaraz Ahmed <imsarfy@gmail.com>
2019-05-07 14:50:01 +0530

    View Products Added

--- view-products.php
+++ view-products.php
@@ -5,9 +5,119 @@
     <?php include('includes/nav.php'); ?>
     <div class="wrapper">
         <div class="heading">View Products</div>
+        <div class="products-container">
+          <div class="product-box">
+            <div class="product-box__image">
+              <img src="img/product-img.jpg" alt="">
+            </div>
+            <div class="product-box__desc">
+              <div class="product-box__name">Product Name</div>
+              <div class="product-box__price">
+                <div class="product-box__currency">&#8377;</div>
+                <div class="product-box__amount">1000</div>
+              </div>
+            </div>
+          </div>
+          <div class="product-box">
+            <div class="product-box__image">
+              <img src="img/product-img.jpg" alt="">
+            </div>
+            <div class="product-box__desc">
+              <div class="product-box__name">Product Name</div>
+              <div class="product-box__price">
+                <div class="product-box__currency">&#8377;</div>
+                <div class="product-box__amount">1000</div>
+              </div>
+            </div>
+          </div>
+          <div class="product-box">
+            <div class="product-box__image">
+              <img src="img/product-img.jpg" alt="">
+            </div>
+            <div class="product-box__desc">
+              <div class="product-box__name">Product Name</div>
+              <div class="product-box__price">
+                <div class="product-box__currency">&#8377;</div>
+                <div class="product-box__amount">1000</div>
+              </div>
+            </div>
+          </div>
+          <div class="product-box">
+            <div class="product-box__image">
+              <img src="img/product-img.jpg" alt="">
+            </div>
+            <div class="product-box__desc">
+              <div class="product-box__name">Product Name</div>
+              <div class="product-box__price">
+                <div class="product-box__currency">&#8377;</div>
+                <div class="product-box__amount">1000</div>
+              </div>
+            </div>
+          </div>
+          <div class="product-box">
+            <div class="product-box__image">
+              <img src="img/product-img.jpg" alt="">
+            </div>
+            <div class="product-box__desc">
+              <div class="product-box__name">Product Name</div>
+              <div class="product-box__price">
+                <div class="product-box__currency">&#8377;</div>
+                <div class="product-box__amount">1000</div>
+              </div>
+            </div>
+          </div>
+          <div class="product-box">
+            <div class="product-box__image">
+              <img src="img/product-img.jpg" alt="">
+            </div>
+            <div class="product-box__desc">
+              <div class="product-box__name">Product Name</div>
+              <div class="product-box__price">
+                <div class="product-box__currency">&#8377;</div>
+                <div class="product-box__amount">1000</div>
+              </div>
+            </div>
+          </div>
+          <div class="product-box">
+            <div class="product-box__image">
+              <img src="img/product-img.jpg" alt="">
+            </div>
+            <div class="product-box__desc">
+              <div class="product-box__name">Product Name</div>
+              <div class="product-box__price">
+                <div class="product-box__currency">&#8377;</div>
+                <div class="product-box__amount">1000</div>
+              </div>
+            </div>
+          </div>
+          <div class="product-box">
+            <div class="product-box__image">
+              <img src="img/product-img.jpg" alt="">
+            </div>
+            <div class="product-box__desc">
+              <div class="product-box__name">Product Name</div>
+              <div class="product-box__price">
+                <div class="product-box__currency">&#8377;</div>
+                <div class="product-box__amount">1000</div>
+              </div>
+            </div>
+          </div>
+          <div class="product-box">
+            <div class="product-box__image">
+              <img src="img/product-img.jpg" alt="">
+            </div>
+            <div class="product-box__desc">
+              <div class="product-box__name">Product Name</div>
+              <div class="product-box__price">
+                <div class="product-box__currency">&#8377;</div>
+                <div class="product-box__amount">1000</div>
+              </div>
+            </div>
+          </div>
+        </div>
     </div>
 
     <!-- <script src="js/main.js"></script> -->
-    <script src="js/add-product.js"></script>
+    <!-- <script src="js/add-product.js"></script> -->
   </body>
 </html>
